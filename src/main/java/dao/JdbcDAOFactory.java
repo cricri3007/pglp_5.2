b9788c0cdf835a7253d9abe90ab2c3646bf5e96a
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-05-24 19:22:46 +0200

    Update JdbcDAOFactory.java

--- src/main/java/dao/JdbcDAOFactory.java
+++ src/main/java/dao/JdbcDAOFactory.java
@@ -6,10 +6,9 @@ public class JdbcDAOFactory extends AbstractDAOFactory {
 	public DAO<Personnel> getPersonnelDAO() {
 		return new PersonnelJdbcDAO();
 	}
-
+	
 	@Override
 	public DAO<PersonnelGroupe> getPersonnelGroupeDAO() {
 		return new PersonnelGroupeJdbcDAO();
 	}
-
 }
